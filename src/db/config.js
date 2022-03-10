855523ca18df6b001953622894d976c993c8c6bd
Baptiste Seux <seuxbaptiste@gmail.com>
2022-03-10 18:13:55 +0100

    test

--- src/db/config.js
+++ src/db/config.js
@@ -5,13 +5,14 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    url:'postgres://kmswtpdkbbvtzd:33213cd29ff256fc59753291b17ee9327d05de9779bf852bc30a71b92c16c1dc@ec2-3-212-45-192.compute-1.amazonaws.com:5432/d18b5hcup39vac',
+    // use_env_variable: "DATABASE_DEV_URL",
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false,
+    //   },
+    // },
   },
 };
