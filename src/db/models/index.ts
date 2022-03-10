d44793eed59a62a1aadf9392dbdc619a2b2f3985
Baptiste Seux <seuxbaptiste@gmail.com>
2022-03-10 11:55:33 +0100

    try to fix sequelize module config not found

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -5,7 +5,9 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const customConfig = require(__dirname + "/../config.js")[env];
+console.log("eeenv: ", env);
+const customConfig = require(__dirname + "/../config")[env];
+console.log(customConfig);
 const db: { sequelize: any; Sequelize: any } = {
   sequelize: undefined,
   Sequelize: undefined,
