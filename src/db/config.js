5c2bf45d2b129bc255e3d41ffcdfe0452e515148
Baptiste Seux <seuxbaptiste@gmail.com>
2022-03-10 15:26:39 +0100

    fix db

--- src/db/config.js
+++ src/db/config.js
@@ -5,8 +5,7 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    database: "im-lazy",
-    use_env_variable: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
