b9721f5a04b752bafd408e3be21c9c1f1c5d4b86
Baptiste Seux <seuxbaptiste@gmail.com>
2022-04-12 16:42:48 +0200

    fix: sequelize path

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,18 +1,17 @@
 "use strict";
 
+// import path from "path";
 import { Sequelize } from "sequelize-typescript";
+import configs from "../config";
 import { File } from "./File";
 import { Payment } from "./Payment";
 import { User } from "./User";
-const env = process.env.NODE_ENV || "development";
-const config = require("../config")[env];
 
-const sequelize = new Sequelize(
-  "im-lazy-development",
-  config.username,
-  config.password,
-  config
-);
+const env = process.env.NODE_ENV || "development";
+
+const config = configs[env];
+
+const sequelize = new Sequelize(process.env[config.use_env_variable], config);
 
 sequelize.addModels([User, File, Payment]);
 
