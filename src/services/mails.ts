5f8d25e0bc53cd021bdcd3183e56084e67e8ea71
Baptiste Seux <seuxbaptiste@gmail.com>
2022-04-12 17:10:26 +0200

    fix: verification account link in email

--- src/services/mails.ts
+++ src/services/mails.ts
@@ -62,9 +62,11 @@ export const sendWelcomeCreatedByAdminMail = ({
     html: `<p>Welcome ${capitalizeFirstLetter(name)} on ImLazy app !</p>
                 <p>You'll discover all the lazy ressources available !</p>
                 <p>Here is your password (don't hesisate to change it on your profile page): ${randomPassword}</p>
-                <p>Last step to verify your account, press <a href="${
-                  process.env.API_URL
-                }:${process.env.PORT}/verify/${verificationString}">Here</a></p>
+                <p>Last step to verify your account, press 
+                <a href="${process.env.API_URL}${
+      process.env.NODE_ENV === "production" ? "" : `:${process.env.PORT}`
+    }:/verify/${verificationString}">Here</a>
+                </p>
                 <p>Enjoy</p>
                 <p>The ImLazy Team</p>`,
   });
@@ -87,6 +89,7 @@ export const sendResetPasswordMail = ({
     subject: "ImLazy app password!",
     html: `<p>Hi ${capitalizeFirstLetter(name)},</p>
           <p>Here is your new password (don't hesisate to change it on your profile page): ${randomPassword}</p>
+          <a href=${process.env.ALLOWED_DOMAIN}>ImLazy app</a>
           <p>Enjoy</p>
           <p>The ImLazy Team</p>`,
   });
